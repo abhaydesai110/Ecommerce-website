f51e4d9046c7f3f04839b6d3f9c116bc51f551a7
Abhay Desai <abhaydesai110@gmail.com>
2024-06-06 22:38:56 +0530

    data base connected

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,9 +1,13 @@
 const mongoose = require("mongoose")
 
-const mongodburl = "mongodb+srv://vinicreation:FCsEsMTDAFU21Ur4@cluster0.7rvkhzb.mongodb.net/?retryWrites=true&w=majority"
+// const mongodburl = "mongodb+srv://abhayecom:2U7c2c9rineB2mD7@cluster0.7rvkhzb.mongodb.net/?retryWrites=true&w=majority"
+const mongodburl = "mongodb+srv://abhayecom:yZjqnV4oGaxcoHg0@cluster0.nvrzmsu.mongodb.net/"
 
-const connectDb = () => {
-    return mongoose.connect(mongodburl);
+const connectDb = async () => {
+    const response = await mongoose.connect(mongodburl);
+    if (response){
+        console.log("Database Connected Successfully")
+    }
 }
 
-module.exports={connectDb}
+module.exports = { connectDb }
